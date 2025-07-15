d9fd12e713aecc63004be51b7e278d64e0d2151f
AQMAN <aqman@naver.com>
2025-07-15 11:47:50 +0900

    Django 백엔드의 CORS 설정에 운영 도메인 추가

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,6 +164,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://zenithworld.shop",
+    "https://www.zenithworld.shop",
+    "https://api.zenithworld.shop",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
