f7c409deea28d8a3a64959e774a067c045d2b17d
AQMAN <aqman@naver.com>
2025-07-15 15:34:33 +0900

    cryptography 반영

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,7 +101,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "backend" / "static",
+]
 
 # Media files
 MEDIA_URL = '/media/'
